45a9ebfe1cb6de8c82dad0f4de5fa037827e3792
jangelgon <jangelgon@gmail.com>
2021-01-31 14:06:44 +0100

    Create ES.h
    This is a Spanish translation

---
+++ include/translations/ES.h
@@ -0,0 +1,24 @@
+// Make sure no string is longer than the display width
+String languageJSON = R"(
+{
+    "systemTitle": "Sistema de Alarma",
+    "armed_away": "Activo - Fuera",
+    "armed_home": "Activo - Casa",
+    "armed_night": "Activo - Noche",
+    "disarmed": "Inactivo",
+    "pending": "Alarma Pendiente",
+    "triggered": "!!! Alarma !!!",
+    "arming": "Armando...",
+    "requested": "Esperando respuesta",
+    "enterCode": "Introduce el Codigo",
+    "invalidCodeHeader": "Codigo Erroneo",
+    "invalidCodeMessage": "Codigo Rechazado",
+    "connecting": "Conectado al servidor",
+    "currentStateHeader": "Estado Actual",
+    "currentStateMessage": "Sistema Preparado",
+    "lightsTurnOffHeader": "Luz",
+    "lightsTurnOffRequested": "Apagando...",
+    "lightsTurnOffSuccess": "Apagado",
+    "peopleArrivedWelcome": "Bienvenido"
+}
+)";
